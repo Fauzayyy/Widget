b5b142d359c16fd67734c4bb78760760e3dd0759
Fauzayyy <fauzip88@gmail.com>
2022-09-23 23:04:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -35,8 +35,6 @@ var IdUser = 0;
 io.on('connection', (socket) => {
 
 
-
-
     socket.broadcast.emit('daftar users',sessions_users);
     io.emit('daftar user',sessions_users);
     socket.emit('daftar_user',sessions_users);
