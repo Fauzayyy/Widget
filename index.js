6532a199863471b827b7a54c89a88acbc7af4624
Fauzayyy <fauzip88@gmail.com>
2022-09-24 23:26:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -82,11 +82,12 @@ socket.on('message',(msg,to)=>{
 });
 })
 
+
 socket.on('send_img', (msg,to,caption) => {
   console.log('received base64 file from' + to);
   const clientLocalSes = sessions_users.find(sess => sess.username == socket.username);
   const ToDataLocalSes = sessions_users.find(sess => sess.idS == to);
-  socket.broadcast.emit('pesan_img', 
+  socket.broadcast.emit('pesan_img',
       {
         "username": socket.username,
         "base64": msg,
